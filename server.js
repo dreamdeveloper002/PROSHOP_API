62b913221c9096ae75306c6d8c681318ff9cd51a
dreamdeveloper002 <bkosemani1@gmail.com>
2021-03-19 22:46:14 +0100

    initial setup

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+
+const app = express();
+
+dotenv.config()
+
+const PORT = process.env.PORT || 8020
+
+app.listen(PORT, () => {
+    console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+});

ff245c76f3d59058026663020f975e24c2685b37
dreamdeveloper002 <bkosemani1@gmail.com>
2021-03-30 00:04:57 +0100

    authMiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -10,23 +10,19 @@ const protect = asyncHandler(async (req, res, next) => {
          try {
           token = req.headers.authorization.split(' ')[1]
           const decoded = jwt.verify(token, process.env.JWT_SECRET)
-           
-          console.log(decoded);
+         
+          req.user = await User.findById(decoded.id).select('-password')
 
           next()
+
          } catch (error) {
-           
+
+           console.error(error);
+           res.status(401);
+           throw new Error('Not authorized, token failed')
+
          }
        };
-
-
-       if(!token) {
-         res.status(401);
-         throw new Error('Not authorizes, no token');
-       }
-
-
-       next()
 });
 
 

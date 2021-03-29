913f0292d08d1882d2873ff606c3649d5b67be1e
dreamdeveloper002 <bkosemani1@gmail.com>
2021-03-30 00:15:08 +0100

    getUserProfile

--- controller/userController.js
+++ controller/userController.js
@@ -37,8 +37,20 @@ const getUserProfile =  asyncHandler(async (req, res) => {
   
     const user = await User.findById(req.user._id);
  
- 
-     res.send('Success')
+     if(user) {
+      res.json ({
+         _id : user._id,
+         name: user.name,
+         email: user.email, 
+         isAdmin: user.isAdmin
+      });
+     } else {
+
+      res.status(404);
+      throw new Error('User not found')
+
+     }
+   
    }); 
 
 export {
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 
 import { authUser, getUserProfile } from "../controller/userController.js";
+import { protect } from '../middleware/authMiddleware.js';
 
 
 
 router.route('/login').post(authUser);
-router.route('/profile').get(getUserProfile);
+router.route('/profile').get(protect, getUserProfile);
 
 
 

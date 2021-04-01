1c5c191b87b7c1aea6b96dd65aa49314694a1ca3
dreamdeveloper002 <bkosemani1@gmail.com>
2021-04-01 23:30:03 +0100

    hashpassword

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,7 +8,7 @@ import { protect } from '../middleware/authMiddleware.js';
 
 
 router.route('/login').post(authUser);
-router.route('/post').post(registerUser);
+router.route('/').post(registerUser);
 router.route('/profile').get(protect, getUserProfile);
 
 

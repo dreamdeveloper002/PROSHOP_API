ad0fc393b7766a9546823a1de5a8d7f12d84955d
dreamdeveloper002 <bkosemani1@gmail.com>
2021-04-02 23:14:32 +0100

    update profile

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,14 +2,23 @@ import express from 'express';
 const router = express.Router();
 
 
-import { authUser, getUserProfile, registerUser } from "../controller/userController.js";
+import { authUser, getUserProfile, registerUser, updateUserProfile } from "../controller/userController.js";
 import { protect } from '../middleware/authMiddleware.js';
 
 
 
-router.route('/login').post(authUser);
-router.route('/').post(registerUser);
-router.route('/profile').get(protect, getUserProfile);
+router
+     .route('/login')
+     .post(authUser);
+
+router
+    .route('/')
+    .post(registerUser);
+
+router
+      .route('/profile')
+      .get(protect, getUserProfile)
+      .put(protect, updateUserProfile);
 
 
 

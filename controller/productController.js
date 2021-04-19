c3a87d04bd45bc8d161ee3263c680ea5d351f465
dreamdeveloper002 <bkosemani1@gmail.com>
2021-04-19 23:40:29 +0100

    admin create and update product

--- controller/productController.js
+++ controller/productController.js
@@ -40,7 +40,7 @@ const getProductById = asyncHandler( async (req, res) => {
 
 //@desc  Delete a product
 //@route  DELETE /api/products/:id
-//@access Public/Admin
+//@access Private/Admin
 const deleteProduct = asyncHandler( async (req, res) => {
 
   const product =  await Product.findById(req.params.id);
@@ -59,10 +59,72 @@ const deleteProduct = asyncHandler( async (req, res) => {
 
 });
 
+//@desc   Create a product
+//@route  POST /api/products
+//@access Private/Admin
+const createProduct = asyncHandler( async (req, res) => {
+
+  const product = new Product({
+      name : 'Sample name',
+      price: 0,
+      user: req.user._id,
+      image: './images/sample.jpg',
+      brand: 'sample brand',
+      category: 'sample category',
+      countInStock: 0,
+      numReviews: 0,
+      desription: 'Sample description'
+  });
+
+
+  const createdProduct = await product.save()
+
+  res.status(201).json(createdProduct)
+});
+
+
+//@desc   Update a product
+//@route  PUT /api/products/:id
+//@access Private/Admin
+const updateProduct = asyncHandler( async (req, res) => {
+
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock
+  } = req.body
+
+
+  const product = await product.findById(req.params.id);
+
+  if(product) {
+
+    product.name = name
+    product.price = price
+    product.description = description,
+    product.image = image,
+    product.brand = brand,
+    product.category = category,
+    product.countInStock = countInStock
+
+    const updatedProduct = await product.save()
+    res.status(201).json(updatedProduct)
+
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+});
 
 
 export {
    getProductById, 
    getProducts,
-   deleteProduct
+   deleteProduct,
+   createProduct,
+   updateProduct
 }

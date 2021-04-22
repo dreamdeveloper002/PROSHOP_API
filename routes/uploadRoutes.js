db3493c2860321de2ac98feae3ef94734192a9c2
dreamdeveloper002 <bkosemani1@gmail.com>
2021-04-22 23:31:18 +0100

    admin product file uploads bug fix

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router()
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, 'upload')
+    cb(null, 'uploads/')
   },
 
   filename(req, file, cb) {

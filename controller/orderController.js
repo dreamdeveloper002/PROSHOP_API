0a59e0903c4a31cd695664245831fdc5613de1c6
dreamdeveloper002 <bkosemani1@gmail.com>
2021-04-24 23:38:32 +0100

    admin update orders to delivered

--- controller/orderController.js
+++ controller/orderController.js
@@ -97,6 +97,30 @@ const updateOrderToPaid =  asyncHandler(async (req, res) => {
 });
 
 
+//@desc   Update order to delivered
+//@route  PUT /api/orders/:id/deliver
+//@access Private/Admin
+const updateOrderToDelivered =  asyncHandler(async (req, res) => {
+ 
+  const order = await Order.findById(req.params.id)
+  
+  if(order) {
+
+    order.isDelivered = true,
+    order.deliveredAt = Date.now()
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+
+});
+
+
+
 
 //@desc   Get logged in user orders
 //@route  PUT /api/orders/myorders
@@ -112,7 +136,7 @@ const getMyOrders =  asyncHandler(async (req, res) => {
 
 
 //@desc   Get all orders
-//@route  PUT /api/orders
+//@route  GET /api/orders
 //@access Private/Admin
 const getOrders =  asyncHandler(async (req, res) => {
  
@@ -130,5 +154,6 @@ export {
   getOrderById,
   updateOrderToPaid,
   getMyOrders,
-  getOrders
+  getOrders,
+  updateOrderToDelivered
 }
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 
-import { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders, getOrders } from "../controller/orderController.js";
+import { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders, getOrders, updateOrderToDelivered} from "../controller/orderController.js";
 import  { protect, admin } from '../middleware/authMiddleware.js';
 
 
@@ -18,6 +18,9 @@ router.route('/:id')
 
 router.route('/:id/pay')
           .put(protect,updateOrderToPaid);
+
+router.route('/:id/deliver')
+          .put(protect, admin, updateOrderToPaid);
 
 
 export default router 
